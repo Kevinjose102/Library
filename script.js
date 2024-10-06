6c6716b4c96ba289b6e9f6ebace3730fd42f5441
Kevinjose102 <kevinjose2505@gmail.com>
2024-10-06 22:10:29 +0530

    updated css and js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const addBookButton = document.querySelector(".add-book")
 const dialog = document.querySelector("dialog")
+const bookGrid = document.querySelector(".books")
+
 const submitButton = document.querySelector(".submit")
 const cancelButton = document.querySelector(".cancel")
+
 const title = document.querySelector(".title")
 const author = document.querySelector(".author")
 const pages = document.querySelector(".pages")
-const read = document.querySelector("#read")
+const readButtons = document.querySelectorAll(".toggle-button")
+
+const error = document.querySelectorAll(".error")
 
 let inputTitle;
 let inputAuthor;
@@ -23,41 +28,84 @@ function Book(title, author, pages, read){
 }
 
 addBookButton.addEventListener("click", () => {
+    readButtons[1].classList.add("active")
     dialog.showModal();
 })
 
 submitButton.addEventListener("click", (e) =>{
     e.preventDefault()
 
-    if(title.value == "" && author.value == "" && pages.value == ""){
-        console.log("wrong")
+    //all inputs are not filled
+    if(title.value == "" ||  author.value == "" || pages.value == ""){
+        if(title.value == ""){
+            error[0].textContent = "*this is a required field"
+        }
+        else{
+            error[0].textContent = ""
+        }
+
+        if(author.value == ""){
+            error[1].textContent = "*this is a required field"
+        }
+        else{
+            error[1].textContent = ""
+        }
+
+        if(pages.value == ""){
+            error[2].textContent = "*this is a required field"
+        }
+        else{
+            error[2].textContent = ""
+        }
     }
     //to check if a book is already present in the library or not
     else if(myLibrary.some(book => book.title === title.value)){
-        console.log("wrong")
+        error[0].textContent = "*this book already exists"
     }
     else{
         //getting the values
         inputTitle = title.value
         inputAuthor = author.value
         inputPages = pages.value
-        if(read.checked === true){
-            inputRead = "true"
-        }
-        else{
-            inputRead = "false"
-        }
+        //inputRead is alread read from the readButon addeventlistener at the bottom
 
+        //adding the new book to the myLibrary array 
         const dummyBook = new Book(inputTitle, inputAuthor, inputPages, inputRead)
-
         myLibrary[count] = dummyBook;
         count = count + 1
         console.log(myLibrary)
+
+        //creating the new book tile in the grid
+        const book = document.createElement("div");
+        book.classList.add("book")
+
+        //TODO ADD CLASSES TO THE INFO IN THE BOOK DIV
+
+        const bookTitle = document.createElement("div");
+        bookTitle.textContent = inputTitle
+
+        const bookAuthor = document.createElement("div")
+        bookAuthor.textContent = inputAuthor
+
+        const bookPages = document.createElement("div");
+        bookPages.textContent = inputPages
+
+        //putting the new book tile on the BOOKGRID
+        bookGrid.appendChild(book)
+
+        book.appendChild(bookTitle)
+        book.appendChild(bookAuthor)
+        book.appendChild(bookPages)
+
 
         //to clear the inputs when submitted
         title.value = ""
         author.value = ""
         pages.value = ""
+        error.forEach(inputError => {
+            inputError.textContent = ""
+        })
+        readButtons.forEach(btn => btn.classList.remove("active"));
         dialog.close()
     }
 })
@@ -67,5 +115,17 @@ cancelButton.addEventListener("click", () =>{
     title.value = ""
     author.value = ""
     pages.value = ""
+    error.forEach(inputError => {
+        inputError.textContent = ""
+    })
+    readButtons.forEach(btn => btn.classList.remove("active"));
     dialog.close()
+})
+
+readButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        readButtons.forEach(btn => btn.classList.remove("active"));
+        button.classList.add("active")
+        inputRead = button.id
+    })
 })
